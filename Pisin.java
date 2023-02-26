f766bbc4ac76acc08b6fe1b845cc037d9b5b4258
Kuutniemi <jkuutniemi@outlook.com>
2023-02-26 23:46:24 +0200

    done2

--- Pisin.java
+++ Pisin.java
@@ -1,15 +1,22 @@
 public class Pisin {
     public static void main(String[] args) {
+        // check the number of parameters
         if (args.length == 0) {
             System.out.println("Ei parametreja");
             return;
         }
+
+        // the first parameter is the longest
         String pisin = args[0];
+
+        // check each parameter, if it is longer, then it is the longest
         for (int i = 1; i < args.length; i++) {
             if (args[i].length() > pisin.length()) {
                 pisin = args[i];
             }
         }
+
+        // print the longest parameter
         System.out.println("Pisin parametri: " + pisin);
     }
 }
